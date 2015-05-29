ba6841fd7d2da97d411bbce035f497671e4eb6e4
Vincent Castellano <surye80@gmail.com>
2015-05-29 02:29:00 -0700

    Initial work on playerquery

--- nfl.py
+++ nfl.py
@@ -1,4 +1,5 @@
 import plugintypes
+import argparse
 import nflgame
 import nfldb
 
@@ -10,28 +11,48 @@ class NflPlugin(plugintypes.TelegramPlugin):
 
 
     patterns = {
-        "^!topqb (20[0-9]{2})": "top_qb",
+        "^!nfl playerquery (.*)": "nfldb_playerquery",
     }
 
     usage = [
-        "!topqb 20xx: get regular season top 10 qbs for year"
+        "!nfl playerquery .*: TODO: DOCUMENT"
     ]
 
-    def top_qb(self, msg, matches):
-        try:
-            year = int(matches.group(1))
-        except:
-            return "Year is malformed"
+    def nfldb_playerquery(self, msg, matches):
+        args = matches.group(1)
 
-        if year > 2014 or year < 2009:
-            return "Only 2009-2014 supported"
+        parser = argparse.ArgumentParser(description='Query the nfldb for stats', prog="!nfl playerquery")
+        parser.add_argument('-w','--week', dest='week', metavar="WEEK", type=int, help="Week to query")
+        parser.add_argument('-y','--year', dest='year', metavar="YEAR", type=int, default=2014, help="Year to query")
+        parser.add_argument('-p','--position', dest='position', metavar="POSITION", help="Position to query")
+        parser.add_argument('-s','--stat', dest='stat', metavar="STAT", help="Stat to query")
+        parser.add_argument('-c','--count', dest='count', metavar="COUNT", type=int, default=5, help="How many results to return (Max: 20)")
+        parser.add_argument('-t','--team', dest='team', metavar="TEAM", help="Team to query, use short name i.e. NE")
 
-        text = "Top QBs:\n"
+        pargs = parser.parse_args(args.split())
+
+        print(pargs)
+
+        if pargs.position == None and pargs.stat == None:
+            return "Either position, stat, or both must be specified"
+
         db = nfldb.connect()
         q = nfldb.Query(db)
 
-        q.game(season_year=year, season_type='Regular')
-        for pp in q.sort('passing_yds').limit(5).as_aggregate():
-            text += "{}: {}yds\n".format(pp.player, pp.passing_yds)
+        q.game(season_year=pargs.year)
+
+
+        if pargs.position:
+            q.player(position=pargs.position)
+
+        if pargs.team:
+            q.game(team=pargs.team)
+
+        if pargs.stat:
+            q.sort(pargs.stat)
+
+        text = "Results:\n"
+        for pp in q.limit(pargs.count).as_aggregate():
+            text += "{}: {}\n".format(pp.player, getattr(pp, pargs.stat))
 
         return text
